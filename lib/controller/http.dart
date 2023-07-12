dab4bb620e0b047015266f38038c4115011e722e
thdthdYi <130949807+thdthdYi@users.noreply.github.com>
2023-07-12 16:45:11 +0900

    Update http.dart

--- lib/controller/http.dart
+++ lib/controller/http.dart
@@ -9,11 +9,7 @@ import '../model/todayweather.dart';
 import '../model/weekweather.dart';
 import 'controller.dart';
 
-const hourapiKey =
-    '7jA2K24VM4wbe1kvwq74KhnqW1zYuiMHuKQciA6pMDmRBUddYnMQjp1IeSWYOv6jydlg/ykaucEbPuGFAQcE0Q==';
 
-const weekapiKey =
-    '7jA2K24VM4wbe1kvwq74KhnqW1zYuiMHuKQciA6pMDmRBUddYnMQjp1IeSWYOv6jydlg%2FykaucEbPuGFAQcE0Q%3D%3D&';
 
 //const apiKey = 'b05bacb845e2db9ffe330721e09c7d02';
 
@@ -27,11 +23,8 @@ class weatherAPI {
   static var client = http.Client();
 
   static Future<Todayweather?> fetchTodayWeatherData(lat, lon) async {
-    String uri =
-        'http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst?serviceKey=7jA2K24VM4wbe1kvwq74KhnqW1zYuiMHuKQciA6pMDmRBUddYnMQjp1IeSWYOv6jydlg/ykaucEbPuGFAQcE0Q==&numOfRows=500&pageNo=1&base_date=20230711&base_time=2300&nx=38&ny=127&dataType=JSON';
-    //'https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst?serviceKey=7jA2K24VM4wbe1kvwq74KhnqW1zYuiMHuKQciA6pMDmRBUddYnMQjp1IeSWYOv6jydlg/ykaucEbPuGFAQcE0Q==&numOfRows=500&pageNo=1&base_date=20230711&base_time=2300&nx=38&ny=127&dataType=JSON';
-    //'http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst?serviceKey=$hourapiKey&numOfRows=900&pageNo=1&base_date=${baseddate - 1}&base_time=2300&nx=$lat&ny=$lon&dataType=JSON';
-    //'http://apis.data.go.kr/1360000/MidFcstInfoService/getMidLandFcst?serviceKey=7jA2K24VM4wbe1kvwq74KhnqW1zYuiMHuKQciA6pMDmRBUddYnMQjp1IeSWYOv6jydlg%2FykaucEbPuGFAQcE0Q%3D%3D&numOfRows=100&pageNo=1&regId=11B00000&tmFc=202307120600&dataType=JSON';
+    String uri ='';
+        
     print('getting todaydata');
 
     var response = await client.get(Uri.parse(uri));
@@ -49,7 +42,7 @@ class weatherAPI {
 
   static Future<WeekWeather?> fetchWeekWeatherData(lat, lon) async {
     String uri =
-        'http://apis.data.go.kr/1360000/MidFcstInfoService/getMidLandFcst?serviceKey=${weekapiKey}numOfRows=100&pageNo=1&regId=11B00000&tmFc=${baseddate}0600&dataType=JSON';
+     '';
 
     print('getting weekdata');
 
